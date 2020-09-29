5e6c1ce8c9f9892e0b39b22e8623d6a9979463e7
Tom Shephard <tom.shephard@hotmail.co.uk>
2020-09-29 09:40:54 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -39,7 +39,7 @@ end
 
 def play (board)
           turn = 0
-    until turn(board) == turn(board)[9] do
+    until turn(board) == turn(board) = 9 do
           turn(board)
           puts "This is number #{turn}"
       turn += 1
